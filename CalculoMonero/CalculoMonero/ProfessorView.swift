3fa60695157102a4fa92041ffc2504437b054b6c
abouchan01 <43842066+abouchan01@users.noreply.github.com>
2018-11-15 17:43:52 -0600

    Add files via upload

---
+++ CalculoMonero/CalculoMonero/ProfessorView.swift
@@ -0,0 +1,24 @@
+//
+//  ProfessorView.swift
+//  calculoMonero
+//
+//  Created by Bouchan on 11/15/18.
+//  Copyright © 2018 Abraham Bouchan . All rights reserved.
+//
+
+import UIKit
+
+class ProfessorView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  /*Hola profesor, esta es unicamente una vista hecha para que el compilador pueda leer el proyecto y mandarlo como tal, espero que la aplicacion le guste.
+ Hello professor, this is only a view made so that the compiler can read the project and send it as such, I hope that the application likes it.
+ */
+
+}
